e99e7216a30333bedd938a794624bcf2c9b0b339
Mustaque Nadim <mustaquenadim2000@gmail.com>
2023-04-01 12:15:28 +0600

    [Complete] Top Collection and Featured Collection Layout Component

--- components/home/FeaturedCollection.jsx
+++ components/home/FeaturedCollection.jsx
@@ -1,15 +1,69 @@
 import { integralCF } from '@/assets/fonts';
-import React from 'react';
+import Image from 'next/image';
+
+import Abstract1 from '@/assets/images/abstract-1.jpg';
+import Abstract2 from '@/assets/images/abstract-2.jpg';
+import Abstract3 from '@/assets/images/abstract-3.jpg';
+import Abstract4 from '@/assets/images/abstract-4.jpg';
+// import Abstract5 from '@/assets/images/abstract-5.jpg';
+import Abstract6 from '@/assets/images/abstract-6.jpg';
+import Abstract7 from '@/assets/images/abstract-7.jpg';
+import Abstract8 from '@/assets/images/abstract-8.jpg';
+import Abstract9 from '@/assets/images/abstract-9.jpg';
+import Artist1 from '@/assets/images/artist-1.png';
+import Button from '../ui/Button';
 
 const FeaturedCollection = () => {
 	return (
-		<section>
+		<section className="pt-[4.0625rem] pb-[5.9375rem] bg-[#D9E0EC] bg-opacity-20">
 			<div className="container">
 				<h2
 					className={`${integralCF.className} font-bold text-[1.75rem] leading-[2.1rem]`}
 				>
 					Collection Featured NFTs
 				</h2>
+				<div className="mt-[3.4375rem] grid grid-cols-3 gap-[1.9375rem]">
+					<div className="space-y-7">
+						<div className="grid grid-cols-[265px_minmax(103.59px,_1fr)] gap-2">
+							<Image
+								src={Abstract9}
+								alt="Abstract Image "
+								className="row-span-3 w-[265px] h-[272.27px]"
+							/>
+							<Image
+								src={Abstract7}
+								alt="Abstract Image "
+								className="h-[85.26px] rounded-[0.6875rem]"
+							/>
+							<Image
+								src={Abstract6}
+								alt="Abstract Image "
+								className="h-[85.26px] rounded-[0.6875rem]"
+							/>
+							<Image
+								src={Abstract3}
+								alt="Abstract Image "
+								className="h-[85.26px] rounded-[0.6875rem]"
+							/>
+						</div>
+						<div>
+							<h3 className="text-xl font-bold">Amazing Collection</h3>
+							<div className="flex items-center justify-between">
+								<div className="flex items-center space-x-2.5">
+									<Image
+										src={Artist1}
+										alt="Artist Image 1"
+										className="w-7 h-7 rounded-full"
+									/>{' '}
+									<div className="font-medium text-sm">by Arkhan</div>
+								</div>
+								<Button className={''}>Total 54 Items</Button>
+							</div>
+						</div>
+					</div>
+					<div></div>
+					<div></div>
+				</div>
 			</div>
 		</section>
 	);
--- components/home/TopCollection.jsx
+++ components/home/TopCollection.jsx
@@ -5,62 +5,98 @@ import Artist1 from '@/assets/images/artist-1.png';
 import Artist10 from '@/assets/images/artist-10.png';
 import Button from '../ui/Button';
 import EthereumIcon from '@/assets/icons/EthereumIcon';
+import VerifiedBadge from '@/assets/icons/verified-badge.svg';
+import { integralCF } from '@/assets/fonts';
 
 const TopCollection = () => {
 	return (
-		<section className="container">
-			<div className="flex">
-				<div className="card w-[25rem] space-y-8">
-					<Image src={Abstract9} alt="Abstract Image 9" />
-					<div className="flex justify-between">
-						<div className="flex items-start ">
-							<Image src={Artist1} alt="Artist Image 1" />
-							<div className="ml-2.5">
-								<div className="font-bold text-xl">The Futr Abstr</div>
-								<small className="mt-1 text-base leading-[1.125rem]">
-									10 in the stock
-								</small>
-							</div>
-						</div>
-						<div className="flex flex-col">
-							<small className="text-[0.75rem] font-medium leading-4">
-								Highest Bid
+		<section className="container py-[8.375rem] flex justify-between">
+			{/* left */}
+			<div className="card w-[25rem] space-y-8">
+				<Image src={Abstract9} alt="Abstract Image 9" />
+				<div className="flex justify-between">
+					<div className="flex items-start ">
+						<Image
+							src={Artist1}
+							alt="Artist Image 1"
+							className="rounded-full"
+						/>
+						<div className="ml-2.5">
+							<div className="font-bold text-xl">The Futr Abstr</div>
+							<small className="mt-1 text-sm leading-[1.125rem] text-[#363639]">
+								10 in the stock
 							</small>
-							<div className="flex items-center font-bold text-base mt-[0.4487rem]">
-								<EthereumIcon className="mr-2.5" /> 0.25 ETH
-							</div>
 						</div>
 					</div>
-				</div>
-				<div>
-					<div className="flex">
-						<div>
-							<Image
-								src={Abstract1}
-								alt="Abstract Image 1"
-								className="w-[147px] h-[147px] rounded-xl"
-							/>
-						</div>
-						<div>
-							<div className="font-bold text-xl">The Futr Abstr</div>
-							<div className="flex items-center mt-2">
-								<Image src={Artist1} alt="Artist Image 1" />{' '}
-								<button className="p-2 border border-[#00AC4F] text-[#00AC4F] rounded-[0.375rem] flex items-center">
-									<EthereumIcon /> 0.25 ETH
-								</button>{' '}
-								1 of 8
-							</div>
-							<Button className={'mt-4'}>Place a bid</Button>
+					<div className="flex flex-col text-[#3A3A3A]">
+						<small className="text-[0.75rem] font-medium leading-4">
+							Highest Bid
+						</small>
+						<div className="flex items-center font-bold text-base mt-[0.4487rem]">
+							<EthereumIcon className="w-[0.7719rem] h-[1.3331rem] mr-2.5" />{' '}
+							0.25 ETH
 						</div>
 					</div>
 				</div>
 			</div>
+			{/* mid */}
 			<div>
-				<h2>Top Collections over</h2>
-				<p>Last 7 days</p>
+				<div className="flex space-x-5">
+					<div>
+						<Image
+							src={Abstract1}
+							alt="Abstract Image 1"
+							className="w-[147px] h-[147px] rounded-xl"
+						/>
+					</div>
+					<div>
+						<div className="font-bold text-xl">The Futr Abstr</div>
+						<div className="flex items-center mt-2 space-x-2.5">
+							<div className="flex items-center space-x-2">
+								<Image
+									src={Artist1}
+									alt="Artist Image 1"
+									className="w-[2.125rem] h-[2.125rem] rounded-full"
+								/>
+								<button className="p-2 border border-[#00AC4F] text-[#00AC4F] text-sm font-bold rounded-[0.375rem] flex items-center">
+									<EthereumIcon className="w-[0.5331rem] h-full mr-[0.3125rem]" />{' '}
+									0.25 ETH
+								</button>
+							</div>
+							<div className="text-sm text-[#757575]">1 of 8</div>
+						</div>
+						<Button className={'mt-4'}>Place a bid</Button>
+					</div>
+				</div>
+			</div>
+			{/* right */}
+			<div>
+				<h2 className={`${integralCF.className} text-xl font-bold`}>
+					Top Collections over
+				</h2>
+				<p className="text-[#3D00B7] font-bold text-lg mt-2.5">Last 7 days</p>
 
-				<div>
-					<Image src={Artist10} alt="Artist Image 10" />
+				<div className="mt-8">
+					<div className="w-[19.5625rem] flex items-center space-x-[1.375rem]">
+						<div className="text-2xl font-bold">1</div>
+						<div className="relative">
+							<Image src={Artist10} alt="Artist Image 10" />
+							<Image
+								src={VerifiedBadge}
+								alt="Verified Badge"
+								className="absolute -top-1 -right-1"
+							/>
+						</div>
+						<div>
+							<div className="text-base leading-[1.0212rem] font-medium -tracking-wide">
+								CryptoFunks
+							</div>
+							<div className="flex items-center text-sm font-semibold -tracking-wide text-[#636363] mt-2.5">
+								<EthereumIcon className="mr-[0.3987rem] text-black" /> 19,769.39
+							</div>
+						</div>
+						<div className="text-[#14C8B0] font-semibold text-xl">+26.52%</div>
+					</div>
 				</div>
 			</div>
 		</section>
